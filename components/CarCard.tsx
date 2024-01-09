0497b26cfa14d66a09118c1ae4a14bed37aa405f
jack rushton <jackrushton96@hotmail.co.uk>
2024-01-09 17:09:45 +0000

    added popup modal to view more button, data from ninja car api

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,32 +1,85 @@
 "use client";
 
-import { useState } from 'react'
-import Image from 'next/image';
-import { CarProps } from '@/types'
+import { useState } from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+import { calculateCarRent } from "@/utils";
+import { CarDetails, CustomButton } from ".";
 
 interface CarCardProps {
-    car: CarProps;
+  car: CarProps;
 }
 
 const CarCard = ({ car }: CarCardProps) => {
-
-    const { city_mpg, year, make, model, transmission, drive } = car;
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='car-card group'>
-        <div className='car-card content'>
-            <h2 className='car-card__content-title'>
-               {make} {model} 
-            </h2>
+    <div className="car-card group">
+      <h2 className="car-card__content-title">
+        {make} {model}
+      </h2>
+
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        ${carRent}
+        <span className="self-end text-[14px] font-medium"> /day</span>
+      </p>
+
+      <div className="relative w-fell h-40 my-3 m-auto object-contain">
+        <Image
+          src="/hero.png"
+          width={280}
+          height={120}
+          alt="car-model"
+          priority
+          className="object-contain"
+        />
+      </div>
+
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering-wheel"
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
         </div>
 
-        <p>
-            <span>
-                Car Rent...
-            </span>
-        </p>
-    </div>
-  )
-}
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => {
+              setIsOpen(true);
+            }}
+          />
+        </div>
+      </div>
 
-export default CarCard
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
+    </div>
+  );
+};
+
+export default CarCard;
